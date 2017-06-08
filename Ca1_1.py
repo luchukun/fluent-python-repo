e266bdcedaa2694a8845e30f63a1c29f9fda51b8
Chuck Lu <luchukun1993@gmail.com>
2017-06-09 00:45:14 +0800

    the demo presented in chapter1

---
+++ Ca1_1.py
@@ -0,0 +1,16 @@
+import collections
+Card = collections.namedtuple("Card",['rank','suit'])
+
+class FrenchDeck:
+    ranks = [str(i) for i in range(2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank,suit) for rank in self.ranks 
+                                       for suit in self.suits]
+    def __len__(self):
+        return len(self._cards)
+    def __getitem__(self, position):
+        return self._cards[position]
+
+        
